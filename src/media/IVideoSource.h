0b124cf956bf57fbefd53fde2d2e322bc499a478
loki2001 <embedded2001@gmail.com>
2025-08-21 17:33:48 +0900

    ADD. IVideoSource Interface Implementation

---
+++ src/media/IVideoSource.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include "../media/ThreadSafeQueue.h"
+#include "../media/VideoFrame.h"
+#include "../media/AudioFrame.h"
+#include "../media/CodecInfo.h"
+
+class IVideoSource {
+public:
+    virtual ~IVideoSource() = default;
+
+    virtual void start() = 0;
+
+    virtual void stop() = 0;
+
+    virtual void flush() = 0;
+
+    virtual bool seek(double timeInSeconds) = 0;
+
+    virtual double getDuration() const = 0;
+
+    virtual CodecInfo getCodecInfo() const = 0;
+
+    virtual ThreadSafeQueue<VideoFrame> &getVideoQueue() = 0;
+
+    virtual ThreadSafeQueue<AudioFrame> &getAudioQueue() = 0;
+};
