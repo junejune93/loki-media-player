f8ab2368475c6c17fcc60b511b79b394ee3d53cc
loki2001 <embedded2001@gmail.com>
2025-08-23 13:00:52 +0900

    FEAT. docker(app) running shellscript implemented to add storage

---
+++ build_loki_media_player_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "=== Building loki-media-player application image ==="
+
+sudo docker build -f loki-media-player.dockerfile -t loki-media-player:latest .
+
+echo "=== Build complete: loki-media-player:latest ==="
