bfcc89cedbd612564bece201e078f60b8858e0bf
loki2001 <embedded2001@gmail.com>
2025-08-23 13:09:58 +0900

    FEAT. Implementation of a shell script that executes docker run commands (convenience)

---
+++ docker-run-loki-media-player.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+xhost +si:localuser:$(whoami)
+
+PULSE_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/run/user/$(id -u)}
+
+MEDIA_DIR="/path/to/media"
+LOGS_DIR="/path/to/logs"
+IMAGE_NAME="loki-media-player:latest"
+
+docker run -it --rm \
+    --net=host \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -e LIBGL_ALWAYS_INDIRECT=0 \
+    -e PULSE_SERVER=unix:$PULSE_RUNTIME_DIR/pulse/native \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $PULSE_RUNTIME_DIR/pulse/native:$PULSE_RUNTIME_DIR/pulse/native \
+    -v $MEDIA_DIR:/app/media \
+    -v $LOGS_DIR:/app/logs \
+    $IMAGE_NAME
