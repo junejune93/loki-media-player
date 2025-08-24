85b248606ad7eee75a3aa347561fa59650f55d7b
loki2001 <embedded2001@gmail.com>
2025-08-25 04:51:03 +0900

    ADD. Resolve X11 Auth in some environments

--- docker-run-loki-media-player.sh
+++ docker-run-loki-media-player.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-xhost +si:localuser:$(whoami)
-
+xhost +local:docker
 PULSE_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/run/user/$(id -u)}
-
 MEDIA_DIR="/path/to/media"
 LOGS_DIR="/path/to/logs"
 IMAGE_NAME="loki-media-player:latest"
@@ -14,8 +11,11 @@ sudo docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     -e LIBGL_ALWAYS_INDIRECT=0 \
     -e PULSE_SERVER=unix:$PULSE_RUNTIME_DIR/pulse/native \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v $PULSE_RUNTIME_DIR/pulse/native:$PULSE_RUNTIME_DIR/pulse/native \
     -v $MEDIA_DIR:/app/media \
     -v $LOGS_DIR:/app/logs \
     $IMAGE_NAME
+
+# Clean up after
+xhost -local:docker
