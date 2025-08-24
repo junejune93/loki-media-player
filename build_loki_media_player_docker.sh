523195d610b46a8d9f548a97ddf86c2f3a6cf8c2
loki2001 <embedded2001@gmail.com>
2025-08-24 14:01:58 +0900

    CHANGE. modify docker(app) file name

--- build_loki_media_player_docker.sh
+++ build_loki_media_player_docker.sh
@@ -2,6 +2,6 @@
 
 echo "=== Building loki-media-player application image ==="
 
-sudo docker build -f loki-media-player.dockerfile -t loki-media-player:latest .
+sudo docker build -f loki-media-player-app.dockerfile -t loki-media-player:latest .
 
 echo "=== Build complete: loki-media-player:latest ==="
